d92bef0ad7785d29390899a75d3f5c1e69496a02
haninggrk <86781236+haninggrk@users.noreply.github.com>
2021-10-22 11:17:16 +0700

    welcome update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,21 @@
 
 <x-custom>
-    <x-slot name="content">aa</x-slot>
+    <x-slot name="content">
+        <div class="absolute right-0" style="top:125px"><img width=800px src="{{asset('img/vector-welcome.png')}}"></div>
+        <div class="flex flex-wrap -mx-3 overflow-hidden">
+
+            <div class="my-3 px-3 w-full overflow-hidden">
+              <h1>Don't wait, this is the time to give</h1>
+            </div>
+          
+            <div class="my-3 px-3 w-full overflow-hidden">
+              <!-- Column Content -->
+            </div>
+          
+            <div class="my-3 px-3 w-full overflow-hidden">
+              <!-- Column Content -->
+            </div>
+          
+          </div>
+    </x-slot>
 </x-custom>
