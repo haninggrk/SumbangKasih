e58241d2242b95082e7fc10097a01ebf3aa2cd76
Nur Azizah <nazizah01@student.ciputra.ac.id>
2021-10-24 08:19:46 +0800

    FINISH BACKEND DASHBOARD ASI

--- app/Http/Livewire/DashboardDonasi.php
+++ app/Http/Livewire/DashboardDonasi.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Models\AsiBoard;
 use App\Models\AsiProduct;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class DashboardDonasi extends Component
@@ -11,6 +12,8 @@ class DashboardDonasi extends Component
     public function render()
     {
         return view('livewire.dashboard-donasi')->with([
-            'getAllAsiProductDashboardRequest' => AsiProduct::where('progress','1')]);
+            'getAllAsiProductDashboardRequest' => AsiProduct::where('progress','1')->where('user_id', Auth::user()->id)->get(),
+            'getAllAsiProductDashboardHistory' => AsiProduct::whereBetween('progress', [2, 3])->where('user_id', Auth::user()->id)->get(),
+            'getAllAsiProductDashboardInProgress'=> AsiProduct::where('progress', '0')->where('user_id', Auth::user()->id)->get()]);
     }
 }
--- resources/views/livewire/dashboard-donasi.blade.php
+++ resources/views/livewire/dashboard-donasi.blade.php
@@ -1,7 +1,32 @@
 
 
+<p>getAllAsiProductDashboardRequest</p>
    @foreach($getAllAsiProductDashboardRequest as $DataAsiRequest)
 
 <p>Nama User Pemilik: {{ $DataAsiRequest->pemilik->name}}</p>
+<p>Nama User Penerima: {{ $DataAsiRequest->receiver->name }}</p>
+<p>Tanggal Melahirkan Pemilik: {{$DataAsiRequest->tanggal_melahirkan  }}</p>
+@endforeach
 
+<p>getAllAsiProductDashboardHistory</p>
+
+<br><br><br><br>
+<p>getAllAsiProductDashboardHistory</p>
+   @foreach($getAllAsiProductDashboardHistory as $DataAsiHistory)
+
+<p>Nama User Pemilik: {{ $DataAsiHistory->pemilik->name}}</p>
+<p>Nama User Penerima: {{ $DataAsiHistory->receiver->name }}</p>
+<p>Tanggal Melahirkan Pemilik: {{$DataAsiHistory->tanggal_melahirkan  }}</p>
+<p>Progress: {{ $DataAsiHistory->progress }}</p>
+@endforeach
+
+<br><br><br><br>
+<p>getAllAsiProductDashboardInProgress</p>
+   @foreach($getAllAsiProductDashboardInProgress as $DataAsiInProgress)
+
+<p>Nama User Pemilik: {{ $DataAsiInProgress->pemilik->name}}</p>
+<p>ID User Pemilik: {{ $DataAsiInProgress->pemilik->id}}</p>
+<p>Nama User Penerima: {{ $DataAsiInProgress->receiver->name }}</p>
+<p>Tanggal Melahirkan Pemilik: {{$DataAsiInProgress->tanggal_melahirkan  }}</p>
+<p>Progress: {{ $DataAsiInProgress->progress }}</p>
 @endforeach
