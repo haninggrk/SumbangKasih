451ae4f28aa4e72f831a3ce64bd8268246a2dee9
Nur Azizah <nur.azizah.7241@gmail.com>
2021-11-18 15:42:34 +0800

    fix back end tmpilan permintaan asi

--- resources/views/livewire/dashboard-donasi.blade.php
+++ resources/views/livewire/dashboard-donasi.blade.php
@@ -30,37 +30,6 @@
 @endif
 @endforeach
 
-
-
-<p>getAllAsiProductDashboardHistory</p>
-
-
-@foreach($DataResipienAsi as $DataResipien)
-
-@if($DataResipien->pivot->progress == 2 || $DataResipien->pivot->progress == 3)
-<form action="{{route('DetailDashboardPendonor-HistoriAsi')}}" method="post">
-    @csrf
-<p>Nama Resipien: {{$DataResipien->name}}</p>
-
-<p>Courir Request: {{$DataResipien->pivot->courir_request}}</p>
-
-<p>Quantity Request: {{$DataResipien->pivot->quantity_request}}</p>
-<p>Detail address resipien Request: {{$DataResipien->pivot->detail_address_resipien}}</p>
-
-<p>Progress: {{$DataResipien->pivot->progress}}</p>
-<input hidden name="asiBoardId" value="{{$DataResipien->pivot->id}}">
-<input hidden name="asiId" value="{{$DataResipien->asiResipiens[0]->id}}">
-<br><br><br>
-<button type="submit" class="btn btn-primary">See</button>
-
-</form>
-@endif
-@endforeach
-
-
-
-
-
 <p>getAllAsiProductDashboardInProgress</p>
 
 @foreach($DataResipienAsi as $DataResipien)
@@ -86,4 +55,97 @@
 </form>
 @endif
 @endforeach
+
+
+<br><br><br>
+/////halaman melihat pesanan asi yang aku pesan sama orang lain
+<p>getAllAsiProductDashboardMenungguRequest</p>
+
+@foreach($DataPermintaanAsi as $DataPermintaan)
+
+@if($DataPermintaan->pivot->progress == 1)
+<form action="/detail-permintaan-request-asi" method="POST">
+    @csrf
+<--- --->
+
+
+<p>Nama pendonor: {{$DataPermintaan->pemilik->name}}</p>
+
+<p>my courir Request: {{$DataPermintaan->pivot->courir_request}}</p>
+
+<p>my Quantity Request: {{$DataPermintaan->pivot->quantity_request}}</p>
+<p>Detail address pendonor: {{$DataPermintaan->detail_address}}</p>
+<p>Progress: {{$DataPermintaan->pivot->progress}}</p>
+<input hidden name="asiBoardId" value="{{$DataPermintaan->pivot->id}}">
+
+<input hidden name="asiId" value="{{$DataPermintaan->id}}">
+
+
+<br><br><br>
+<button type="submit" class="btn btn-primary">See</button>
+
+</form>
+@endif
+@endforeach
+
+
+
+<p>getAllmypesananAsiProductDashboardHistory</p>
+
+
+@foreach($DataPermintaanAsi as $DataPermintaan)
+
+@if($DataPermintaan->pivot->progress == 2 || $DataPermintaan->pivot->progress == 3)
+<form action="/detail-permintaan-histori-asi" method="post">
+    @csrf
+<p>Nama pendonor: {{$DataPermintaan->pemilik->name}}</p>
+
+<p>my Request: {{$DataPermintaan->pivot->courir_request}}</p>
+
+<p>my Quantity Request: {{$DataPermintaan->pivot->quantity_request}}</p>
+<p>Detail address pendonor: {{$DataPermintaan->detail_address}}</p>
+
+<p>Progress: {{$DataPermintaan->pivot->progress}}</p>
+<input hidden name="asiBoardId" value="{{$DataPermintaan->pivot->id}}">
+<input hidden name="asiId" value="{{$DataPermintaan->id}}">
+<br><br><br>
+<button type="submit" class="btn btn-primary">See</button>
+
+</form>
+@endif
+@endforeach
+
+
+
+
+
+<p>getAllmyPesananAsiProductDashboardInProgress</p>
+
+@foreach($DataPermintaanAsi as $DataPermintaan)
+
+@if($DataPermintaan->pivot->progress == 0)
+
+<form action="/detail-permintaan-inprogress-asi" method="post">
+    @csrf
+
+    <p>Nama pendonor: {{$DataPermintaan->pemilik->name}}</p>
+
+<p>my Request: {{$DataPermintaan->pivot->courir_request}}</p>
+
+<p>my Quantity Request: {{$DataPermintaan->pivot->quantity_request}}</p>
+<p>Detail address pendonor: {{$DataPermintaan->detail_address}}</p>
+
+<p>Progress: {{$DataPermintaan->pivot->progress}}</p>
+<input hidden name="asiBoardId" value="{{$DataPermintaan->pivot->id}}">
+<input hidden name="asiId" value="{{$DataPermintaan->id}}">
+
+<br><br><br>
+<button type="submit" class="btn btn-primary">See</button>
+</form>
+@endif
+@endforeach
+
+
+
+
 
